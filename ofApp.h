236cc349d3e57b7b75e4d6832e9bcdb0dd704bb0
Daniel Grounin <69406706+Daniel-Grounin@users.noreply.github.com>
2024-07-27 12:22:55 +0300

    Update ofApp.h

--- ofApp.h
+++ ofApp.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "ofMain.h"
 #include <vector>
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <json/json.h>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 struct SatelliteData {
+    std::string name;
     double latitude;
     double longitude;
     double altitude;
@@ -14,6 +23,12 @@ public:
     void setup();
     void update();
     void draw();
+    void exit();
+
+    void run();  // Function to handle socket communication
+    void cleanupWinsock();
+    void initializeWinsock();
+    std::string receiveData(SOCKET ConnectSocket);
 
     ofSpherePrimitive sphere;
     ofLight light;
@@ -28,6 +43,9 @@ public:
     int currentBatchIndex; // Index to track the current batch
     float lastUpdateTime; // Time of the last update
     float updateInterval; // Interval between updates
+    ofImage backgroundImage; // Background image
 
-    
+private:
+    std::thread socketThread;  // Thread for handling socket communication
+    std::mutex satelliteMutex;
 };
